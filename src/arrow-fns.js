2d57c1c88dbbf9c92128c3d2ffa38a6b2b965d75
Richard Webber <rikki.webber@gmail.com>
2023-11-10 14:12:07 -0700

    arrow-fns-commit

--- src/arrow-fns.js
+++ src/arrow-fns.js
@@ -5,7 +5,7 @@
 // Ex.:
 //   giveMeTwo();
 //   => 2
-const giveMeTwo = undefined;
+const giveMeTwo = () => 2;
 
 // Set the value of addNums to an arrow function that takes in two numbers and
 // returns their sum.
@@ -13,7 +13,7 @@ const giveMeTwo = undefined;
 // Ex.:
 //   addNums(1, 2);
 //   => 3
-const addNums = undefined;
+const addNums = (num1, num2) => num1 + num2;
 
 // Set the value of max to an arrow function that takes in two numbers and
 // returns the largest one.
@@ -21,7 +21,7 @@ const addNums = undefined;
 // Ex.:
 //   max(1, 2);
 //   => 2
-const max = undefined;
+const max = (num1, num2) => (num1 >= num2 ? num1 : num2);
 // Note: you can also do this on one line:
 // const max = (a, b) => (a >= b ? a : b);
 
@@ -31,7 +31,9 @@ const max = undefined;
 // Ex.:
 //   evens([1, 2, 3, 4, 5]);
 //   => [2, 4]
-function evens(nums) {}
+function evens(nums) {
+    return nums.filter(num => num % 2 === 0)
+ }
 
 // Given an array of names, return a new array of greetings for each name. Do
 // this using array iteration methods. Do NOT use a for loop.
@@ -39,7 +41,9 @@ function evens(nums) {}
 // Ex.:
 //   createGreetings(['Clive', 'Jill', 'Torgal']);
 //   => ['Hello, Clive!', 'Hello, Jill!', 'Hello, Torgal!']
-function createGreetings(names) {}
+function createGreetings(names) {
+    return names.map(name => `Hello, ${name}!`)
+}
 
 // Given an array of words, return a new array where each word is uppercased
 // and only includes words that are longer than 4 characters. Do this using
@@ -48,6 +52,11 @@ function createGreetings(names) {}
 // Ex.:
 //   loudLongWords(['apple', 'pear', 'cake', 'pinata']);
 //   => ['APPLE', 'PINATA']
-function loudLongWords(words) {}
+function loudLongWords(words) {
+    let longestWord = 4;
+
+    let longestWordsArray = words.filter(word => word.length > longestWord)
+    return longestWordsArray.map(n => n.toUpperCase())
+  }
 
 export { addNums, createGreetings, evens, giveMeTwo, loudLongWords, max };
